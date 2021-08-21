3e77b901e8bbb949367cb1ccada2dc4f3590c726
Danil Kolumbet <kolumbetko@gmail.com>
2021-08-21 06:10:54 +0300

    Implement nested elements

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const element = React.createElement("div", null, "hello, world")
+const element = React.createElement("div", null,
+    React.createElement("h1", null, "Todo list:"),
+    React.createElement("ul", null,
+        React.createElement("li", null, "hello"),
+        React.createElement("li", null, "world"),
+        [
+            React.createElement("li", null, "This"),
+            React.createElement("li", null, "is"),
+            React.createElement("li", null, "array!")
+        ]
+    ),
+    React.createElement("footer", null, "2021 kolumbetko")
+)
 const container = document.querySelector("#app_container")
 ReactDOM.render(element, container)
--- react.js
+++ react.js
@@ -1,7 +1,38 @@
+const flattenArray = (acc, el) => {
+    if (Array.isArray(el)){
+        return acc.concat(el)
+    } else {
+        acc.push(el)
+        return acc
+    }
+}
+
 const React = {
-    createElement: (tagName, props, text) => {
-        const element = document.createElement(tagName)
-        element.innerText = text
+    createElement: function (type, config, children) {
+        children = Array.from(arguments).slice(2).reduce(flattenArray, [])
+        let element
+        switch (typeof type) {
+            case "string":
+            element = document.createElement(type)
+            children.forEach(child => {
+                switch (typeof child) {
+                    case "string":
+                    const text = document.createTextNode(child)
+                    element.appendChild(text)
+                    break
+                    case "object":
+                    if (child !== null) {
+                        element.appendChild(child)
+                    }
+                    break
+                }
+            })
+            break
+            case "function":
+            const props = {children}
+            element = type(props)
+            break
+        }
         return element
     }
 }
